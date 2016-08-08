61fcd9203aa8701ef6a3253b77494390abaad68f
Neil Lawrence <lawrennd@gmail.com>
2016-08-08 09:13:46 +0200

    Update test example and way of doing environments.

--- python/ndlfilters.py
+++ python/ndlfilters.py
@@ -12,10 +12,12 @@ import json
 filters = ['../../../pandoc/python/myfilter.py']#, 'pandoc-citeproc']
 pdoc_args = ['--mathjax', '--smart', '--parse-raw']
 
-
 def decamel(name):
     """Remove camel case from a string"""
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    #s1 = re.sub('([^0-9]([0-9][0-9])[^0-9]', r'0\1', s1)
+    #s1 = re.sub('([^0-9]([0-9])[^0-9]', r'00\1', s1)
+    #sys.stderr.write(s1)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 def get_filename(source):
@@ -77,24 +79,90 @@ def picture2image(picture_src, filetype, outfile):
         call(["convert", tmpdir + '/picture.pdf', outfile + '.' + filetype])
     #shutil.rmtree(tmpdir)
 
+def get_file(fullfile):
+    """Get a decamelled filename"""
+    base = os.path.basename(fullfile)
+    outfile = decamel(os.path.splitext(base)[0])
+    dirname = os.path.dirname(fullfile)
+    return os.path.join(dirname, outfile)
 
-def inputdiagram(key, val, fmt, meta):
-    if key == 'RawInline':
-        [fmt, code] = val
-        if fmt == "latex" and re.match(r"\\inputdiagram", code):
-            #outfile = get_filename4code("inputdiagram", code)
-            match_macro = re.compile(r"""\\inputdiagram\{(.*)\}""")
-            macro_match = match_macro.findall(code)
-            for m in macro_match:
-                fullfile = m
-                outfile = os.path.splitext(fullfile)[0]
+def include_file(name, ext='.tex'):
+    """Deal with included files."""
+    def fun(key, val, fmt, meta):
+        if key == 'RawInline':
+            [fmt, code] = val
+            if fmt == "latex" and re.match(r"\\{name}".format(name=name), code):
+                macro_pattern = re.compile(r"\\{name}{{(.*)}}".format(name=name))
+                m = macro_pattern.match(code)
+                if m:
+                    body = m.group(1)
+                    sys.stderr.write(body)
+                    f, e = os.path.splitext(body)
+                    if e == '':
+                        body += ext
+                    f = get_file(f)
+                    outputfile = f + '.md'
+                    if body[0] != '#':
+                        if not os.path.isfile(outputfile):
+                            # Call pandoc again on the included file
+                            call(['pandoc',
+                                  '-R', body, '--to=markdown',
+                                  '--filter=../../../pandoc/python/myfilter.py',
+                                  '--from=latex',
+                                  '--output=' + outputfile],
+                                 stdout=sys.stderr)
+                        return Link(['', [], []], [Str(f)], [outputfile, ""])
+    return fun
 
-            filetype = get_extension(format, "png", html="png", latex="pdf")
-            src = outfile + '.' + filetype
-            if not os.path.isfile(src):
-                picture2image(code, filetype, outfile)
-                sys.stderr.write('Created image ' + src + '\n')
-            return Image(['', [], []], [], [src, ""])
+                        
+def environment_replace(name,
+                        preamble='\input{notation_def.tex}',
+                        template='{preamble}\n\n {name} start\n\n {body}\n\n {name} end'):
+    """Process a latex environment."""
+    def fun(key, val, fmt, meta):
+        if key == 'RawBlock':
+            [fmt, code] = val
+            if fmt == "latex":
+                if re.match(r"\\begin{{{name}}}".format(name=name), code):
+                    macro_pattern = re.compile(r"""\\begin{{{name}}}(.*)\\end{{{name}}}""".format(name=name), re.DOTALL)
+                    m = macro_pattern.match(code)
+                    if m:
+                        body = m.group(1)
+                        environment_text = template.format(preamble=preamble, name=name, body=body)
+                        output=pd.convert_text(source=environment_text,
+                                               to='json',
+                                               format='latex',
+                                               extra_args=pdoc_args,
+                                               filters=filters)
+                        return json.loads(output)[1]
+    return fun
+
+def command_replace(name, replace='{name} {body}'):
+    """Process a latex environment."""
+    def fun(key, val, fmt, meta):
+        if key == 'RawInline':
+            [fmt, code] = val
+            if fmt == "latex":
+                if re.match(r"\\{name}".format(name=name), code):
+                    macro_pattern = re.compile(r"""\\{name}{{(.*)}}""".format(name=name), re.DOTALL)
+                    m = macro_pattern.match(code)
+                    if m:
+                        sys.stderr.write(str(m.group(1)))
+                        body = m.group(1)
+                        command_text = replace.format(name=name, body=body)
+                        return Str(command_text)
+    return fun
+
+column = command_replace('column', replace='{{{name} width={body}}}')
+only = command_replace('only', replace='{{{name} slideno={body}}}')
+onslide = command_replace('onslide', replace='{{{name} slideno={body}}}')
+overprint = environment_replace('overprint')
+columns = environment_replace('columns')
+frame = environment_replace('frame')
+widelist = environment_replace('widelist', template='\\begin{{description}}\n{body}\\end{{description}}')
+animateinline = environment_replace('animateinline')
+includetalkfile = include_file('includetalkfile')
+includecvfile = include_file('includecvfile')
 
 def MakeUppercase(key, val, fmt,meta):
     if key == 'RawInline':
@@ -107,75 +175,22 @@ def MakeUppercase(key, val, fmt,meta):
                 M = caps(val, fmt, meta)
             return Str(M)
 
-
-def includetalkfile(key, val, fmt, meta):
-    "Deal with included talk files."
+def inputdiagram(key, val, fmt, meta):
     if key == 'RawInline':
         [fmt, code] = val
-        if fmt == "latex" and re.match(r"\\includetalkfile", code):
-            match_macro = re.compile(r"""\\includetalkfile\{(.*)\}""")
+        if fmt == "latex" and re.match(r"\\inputdiagram", code):
+            #outfile = get_filename4code("inputdiagram", code)
+            match_macro = re.compile(r"""\\inputdiagram\{(.*)\}""")
             macro_match = match_macro.findall(code)
             for m in macro_match:
-                f, ext = os.path.splitext(m)
-                if ext == '':
-                    m += '.tex'
-                outputfile = f + '.md'                    
-                if m[0] != '#':
-                    # Call pandoc again on the included file
-                    call(['pandoc',
-                          '-R', m, '--to=markdown',
-                          '--filter=../../../pandoc/python/myfilter.py',
-                          '--from=latex',
-                          '--output=' + outputfile],
-                         stdout=sys.stderr)
-                    return Link(['', [], []], [Str(f)], [outputfile, ""])
-                        
-
-
-def environment_replace(name, preamble='\input{notation_def.tex}'):
-    """Process a latex environment."""
-    def fun(key, val, fmt, meta):
-        if key == 'RawBlock':
-            [fmt, code] = val
-            if fmt == "latex":
-                if re.match(r"\\begin{{{name}}}".format(name=name), code):
-                    match_macro = re.compile(r"""\\begin{{{name}}}(.*)\\end{{{name}}}""".format(name=name), re.DOTALL)
-                    macro_match = match_macro.findall(code)
-                    for m in macro_match:
-                        body = m
-                        environment_text = '{preamble}\n\n {name} start\n\n {body}\n\n {name} end'.format(preamble=preamble, name=name, body=body)
-                        output=pd.convert_text(source=environment_text,
-                                               to='json',
-                                               format='latex',
-                                               extra_args=pdoc_args,
-                                               filters=filters)
-                        return json.loads(output)[1]
-    return fun
-
-
-overprint = environment_replace('overprint')
-columns = environment_replace('columns')
-frame = environment_replace('frame')
-animateinline = environment_replace('animateinline')
-
-def command_replace(name, replace='{name} {body}'):
-    """Process a latex environment."""
-    def fun(key, val, fmt, meta):
-        if key == 'RawInline':
-            [fmt, code] = val
-            if fmt == "latex":
-                if re.match(r"\\{name}".format(name=name), code):
-                    match_macro = re.compile(r"""\\{name}{{(.*)}}""".format(name=name), re.DOTALL)
-                    macro_match = match_macro.findall(code)
-                    for m in macro_match:
-                        body = m
-                        command_text = replace.format(name=name, body=body)
-                        return Str(command_text)
-    return fun
-
-column = command_replace('column', replace='{{{name} width={body}}}')
-only = command_replace('only', replace='{{{name} slideno={body}}}')
-onslide = command_replace('onslide', replace='{{{name} slideno={body}}}')
+                fullfile = m
+                outfile = get_file(fullfile)
+                filetype = get_extension(format, "png", html="png", latex="pdf")
+                src = outfile + '.' + filetype
+                if not os.path.isfile(src):
+                    picture2image(code, filetype, outfile)
+                    sys.stderr.write('Created image ' + src + '\n')
+                return Image(['', [], []], [], [src, ""])
 
 def overlay(key, value, fmt, meta):
     "From https://andrewgoldstone.com/blog/2014/12/24/slides/"
